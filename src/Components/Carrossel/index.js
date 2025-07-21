e7ec444bbd2258e84701548940cf6baa0526148a
Pedro Henrique de Lima Rosa Pedro <pedroHplayer190@gmail.com>
2025-07-21 20:32:41 -0300

    Test: direto nas imagens

--- src/Components/Carrossel/index.js
+++ src/Components/Carrossel/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Todas_imagens from "./Images";
 import { EffectCards } from "swiper/modules";
+
 function Carrossel() {
   return (
     <div className="container">
@@ -15,11 +16,40 @@ function Carrossel() {
           perSlideOffset: 4,
         }}
       >
-        {Todas_imagens.map((item) => (
+        <SwiperSlide className="sla">
+          <img
+            src={"/images/IMG-20250721-WA0125.jpg"}
+            alt={`Imagem test3`}
+            className="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="sla">
+          <img
+            src={"/images/IMG-20250721-WA0126.jpg"}
+            alt={`Imagem test23`}
+            className="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="sla">
+          <img
+            src={"/images/IMG-20250721-WA0012.jpg"}
+            alt={`Imagem test2`}
+            className="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="sla">
+          <img
+            src={"/images/IMG-20250721-WA0120.jpg"}
+            alt={`Imagem test1`}
+            className="img"
+          />
+        </SwiperSlide>
+
+        {/* {Todas_imagens.map((item) => (
           <SwiperSlide className="sla" key={item.id}>
             <img src={item.image} alt={`Imagem ${item.id}`} className="img" />
           </SwiperSlide>
-        ))}
+        ))} */}
       </Swiper>
     </div>
   );
