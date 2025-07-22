b9c188404df895ea43849b2c7377a0e611ee71fc
Pedro Henrique de Lima Rosa Pedro <pedroHplayer190@gmail.com>
2025-07-21 21:48:07 -0300

    Defined: Acabei tudo graças a Deus

--- src/Components/Carrossel/index.js
+++ src/Components/Carrossel/index.js
@@ -15,36 +15,8 @@ function Carrossel() {
           perSlideRotate: 1,
           perSlideOffset: 4,
         }}
+        // width={200}
       >
-        {/* <SwiperSlide className="sla">
-          <img
-            src={"/IMG-20250721-WA0125.jpg"}
-            alt={`Imagem test3`}
-            className="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="sla">
-          <img
-            src={"/IMG-20250721-WA0126.jpg"}
-            alt={`Imagem test23`}
-            className="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="sla">
-          <img
-            src={"/IMG-20250721-WA0012.jpg"}
-            alt={`Imagem test2`}
-            className="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="sla">
-          <img
-            src={"/IMG-20250721-WA0120.jpg"}
-            alt={`Imagem test1`}
-            className="img"
-          />
-        </SwiperSlide> */}
-
         {Todas_imagens.map((item) => (
           <SwiperSlide className="sla" key={item.id}>
             <img src={item.image} alt={`Imagem ${item.id}`} className="img" />
